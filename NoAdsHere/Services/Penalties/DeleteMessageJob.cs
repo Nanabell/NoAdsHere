e88ea76c6f78fb33377d3a30f0fb00df034d578e
Nanabell <marlon.delfin@googlemail.com>
2017-05-31 14:24:02 +0200

    Add DeleteMessage Job & TriggerQueue

---
+++ NoAdsHere/Services/Penalties/DeleteMessageJob.cs
@@ -0,0 +1,57 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+using Discord;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+
+namespace NoAdsHere.Services.Penalties
+{
+    public static class JobQueue
+    {
+        private static IScheduler _scheduler;
+
+        public static Task Install(IServiceProvider provider)
+        {
+            _scheduler = provider.GetService<IScheduler>();
+
+            return Task.CompletedTask;
+        }
+
+        public static async Task QueueTrigger(IUserMessage message)
+        {
+            var job = JobBuilder.Create<DeleteMessageJob>()
+                .StoreDurably()
+                .Build();
+
+            var trigger = TriggerBuilder.Create()
+                .StartAt(DateTimeOffset.Now.AddSeconds(10))
+                .ForJob(job)
+                .Build();
+            trigger.JobDataMap["message"] = message;
+
+            await _scheduler.ScheduleJob(job, trigger);
+        }
+    }
+
+    public class DeleteMessageJob : IJob
+    {
+        private readonly Logger _logger = LogManager.GetLogger("AntiAds");
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var datamap = context.Trigger.JobDataMap;
+
+            var message = (IUserMessage)datamap["message"];
+
+            try
+            {
+                await message.DeleteAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.Warn(e, $"Unable to delete Message {message.Id}.");
+            }
+        }
+    }
+}
