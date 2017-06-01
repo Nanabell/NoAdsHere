38a3a3e3547effec2bf3b5abb9c093bf80ae9fd5
Nanabell <marlon.delfin@googlemail.com>
2017-06-01 06:13:13 +0200

    Enable the Twitch Blocks part

--- NoAdsHere/Commands/Blocks/BlockModule.cs
+++ NoAdsHere/Commands/Blocks/BlockModule.cs
@@ -24,5 +24,85 @@ namespace NoAdsHere.Commands.Blocks
             else
                 await ReplyAsync($":exclamation: Discord Invite Blocks already set to {setting} :exclamation:");
         }
+
+        [Command("Twitch")]
+        [RequirePermission(AccessLevel.HighModerator)]
+        public async Task Twitch(bool setting)
+        {
+            bool success;
+            if (setting)
+            {
+                var _1 = await AntiAds.TryEnableGuild(BlockType.TwitchClip, Context.Guild.Id);
+                var _2 = await AntiAds.TryEnableGuild(BlockType.TwitchStream, Context.Guild.Id);
+                var _3 = await AntiAds.TryEnableGuild(BlockType.TwitchVideo, Context.Guild.Id);
+                if (_1 && _2 && _3)
+                    success = true;
+                else
+                    success = false;
+            }
+            else
+            {
+                var _1 = await AntiAds.TryDisableGuild(BlockType.TwitchClip, Context.Guild.Id);
+                var _2 = await AntiAds.TryDisableGuild(BlockType.TwitchStream, Context.Guild.Id);
+                var _3 = await AntiAds.TryDisableGuild(BlockType.TwitchVideo, Context.Guild.Id);
+                if (_1 && _2 && _3)
+                    success = true;
+                else
+                    success = false;
+            }
+
+            if (success)
+                await ReplyAsync($":white_check_mark: Twitch Link Blockings have been set to {setting}. {(setting ? "Please ensure that the bot can ManageMessages in the required channels" : "")} :white_check_mark:");
+            else
+                await ReplyAsync($":exclamation: Twitch Link Blocks already set to {setting} :exclamation:");
+        }
+
+        [Command("Twitch Stream")]
+        [RequirePermission(AccessLevel.HighModerator)]
+        public async Task TwitchStream(bool setting)
+        {
+            bool success;
+            if (setting)
+                success = await AntiAds.TryEnableGuild(BlockType.TwitchStream, Context.Guild.Id);
+            else
+                success = await AntiAds.TryDisableGuild(BlockType.TwitchStream, Context.Guild.Id);
+
+            if (success)
+                await ReplyAsync($":white_check_mark: Twitch Stream Blockings have been set to {setting}. {(setting ? "Please ensure that the bot can ManageMessages in the required channels" : "")} :white_check_mark:");
+            else
+                await ReplyAsync($":exclamation: Twitch Stream Blocks already set to {setting} :exclamation:");
+        }
+
+        [Command("Twitch Clip")]
+        [RequirePermission(AccessLevel.HighModerator)]
+        public async Task TwitchClip(bool setting)
+        {
+            bool success;
+            if (setting)
+                success = await AntiAds.TryEnableGuild(BlockType.TwitchClip, Context.Guild.Id);
+            else
+                success = await AntiAds.TryDisableGuild(BlockType.TwitchClip, Context.Guild.Id);
+
+            if (success)
+                await ReplyAsync($":white_check_mark: Twitch Clip Blockings have been set to {setting}. {(setting ? "Please ensure that the bot can ManageMessages in the required channels" : "")} :white_check_mark:");
+            else
+                await ReplyAsync($":exclamation: Twitch Clip Blocks already set to {setting} :exclamation:");
+        }
+
+        [Command("Twitch Video")]
+        [RequirePermission(AccessLevel.HighModerator)]
+        public async Task TwitchVideo(bool setting)
+        {
+            bool success;
+            if (setting)
+                success = await AntiAds.TryEnableGuild(BlockType.TwitchVideo, Context.Guild.Id);
+            else
+                success = await AntiAds.TryDisableGuild(BlockType.TwitchVideo, Context.Guild.Id);
+
+            if (success)
+                await ReplyAsync($":white_check_mark: Twitch Viode Blockings have been set to {setting}. {(setting ? "Please ensure that the bot can ManageMessages in the required channels" : "")} :white_check_mark:");
+            else
+                await ReplyAsync($":exclamation: Twitch Video Blocks already set to {setting} :exclamation:");
+        }
     }
 }
