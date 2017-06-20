6e3e184139686da0fff5db112dc405a0b972d652
Nanabell <marlon.delfin@googlemail.com>
2017-06-20 17:13:29 +0200

    Add Stats db Model

---
+++ NoAdsHere/Database/Models/GuildSettings/Stats.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+
+namespace NoAdsHere.Database.Models.GuildSettings
+{
+    public class Stats : IIndexed
+    {
+        public Stats(ulong guildId)
+        {
+            GuildId = guildId;
+        }
+        public ObjectId Id { get; set; }
+        public ulong GuildId { get; set; }
+        public uint Blocks { get; set; }
+        public uint Warns { get; set; }
+        public uint Kicks { get; set; }
+        public uint Bans { get; set; }
+    }
+}
