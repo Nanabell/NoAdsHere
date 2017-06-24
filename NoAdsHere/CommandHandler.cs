c50df4e7e50c296c8edd1deb86db347d2bd2f484
Nanabell <marlon.delfin@googlemail.com>
2017-06-24 02:34:43 +0200

    better error responds

--- NoAdsHere/CommandHandler.cs
+++ NoAdsHere/CommandHandler.cs
@@ -2,12 +2,15 @@
 using Discord.WebSocket;
 using NLog;
 using System;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using NoAdsHere.Common;
 using NoAdsHere.Services.Configuration;
+using ParameterInfo = Discord.Commands.ParameterInfo;
 
 namespace NoAdsHere
 {
@@ -76,7 +79,7 @@ namespace NoAdsHere
             switch (result)
             {
                 case SearchResult searchResult:
-                    if (searchResult.ErrorReason == "Unknown command")
+                    if (searchResult.Error == CommandError.UnknownCommand)
                     {
                         Logger.Debug($"User {context.User} tried to use a unknown command in {context.Guild}/{context.Channel}");
                         return;
@@ -86,11 +89,15 @@ namespace NoAdsHere
                     break;
 
                 case ParseResult parseResult:
+                    var command = _commands.Search(context, argPos).Commands.First();
                     response = $":warning: There was an error parsing your command: `{parseResult.ErrorReason}`";
+                    response +=
+                        $"\nCorrect Usage is: `{_config.CommandStrings.First()} {command.Alias} {string.Join(" ", command.Command.Parameters.Select(FormatParam)).Replace("`", "")}`";
                     break;
 
                 case PreconditionResult preconditionResult:
                     response = $":warning: A precondition of your command failed: `{preconditionResult.ErrorReason}`";
+                    
                     break;
 
                 case ExecuteResult executeResult:
@@ -121,6 +128,33 @@ namespace NoAdsHere
                 }
             }
             return flag;
+        }
+        
+        private static string FormatParam(ParameterInfo parameter)
+        {
+            var sb = new StringBuilder();
+            if (parameter.IsMultiple)
+            {
+                sb.Append($"`[{parameter.Name}...]`");
+            }
+            else if (parameter.IsRemainder)
+            {
+                sb.Append($"`<{parameter.Name}...>`");
+            }
+            else if (parameter.IsOptional)
+            {
+                sb.Append($"`[{parameter.Name}]`");
+            }
+            else
+            {
+                sb.Append($"`<{parameter.Name}>`");
+            }
+
+            if (!string.IsNullOrWhiteSpace(parameter.Summary))
+            {
+                sb.Append($" ({parameter.Summary})");
+            }
+            return sb.ToString();
         }
     }
 }
