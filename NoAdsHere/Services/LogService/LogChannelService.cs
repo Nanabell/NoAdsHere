ee2fe5c4f7623ad11e83d9d0b349c08eaf88f38b
Nanabell <marlon.delfin@googlemail.com>
2017-07-02 22:04:18 +0200

    Shitty Log .gitIgnore

---
+++ NoAdsHere/Services/LogService/LogChannelService.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Webhook;
+using Discord.WebSocket;
+using NLog;
+using NoAdsHere.Services.Configuration;
+using NoAdsHere.Services.Events;
+
+namespace NoAdsHere.Services.Log
+{
+    public class LogChannelService
+    {
+        private readonly DiscordWebhookClient _client;
+        private readonly Logger _logger = LogManager.GetLogger("Webhook");
+        public LogChannelService(Config config)
+        {
+            _client = new DiscordWebhookClient(config.WebHookLogger.Id, config.WebHookLogger.Token);
+            _client.Log += EventHandlers.WebhookLogger;
+        }
+
+        internal async Task SendMessageAsync(string message)
+        {
+            await _client.SendMessageAsync(message);
+
+        }
+        
+
+        internal async Task LogMessageAsync(DiscordShardedClient client, DiscordSocketClient shard, Emote emote,
+            string message)
+        {
+            await _client.SendMessageAsync($"`[{shard.ShardId:00} / {client.Shards.Count:00}]` " +
+                                           $"<:{emote.Name}:{emote.Id}> " +
+                                           $"{message}", false, null, shard.CurrentUser.Username,
+                shard.CurrentUser.GetAvatarUrl(ImageFormat.Png));
+
+        } 
+    }
+}
