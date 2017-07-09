5c08a17c863e442e2dd34dd438fe63dbb4f25671
Nanabell <marlon.delfin@googlemail.com>
2017-07-09 22:59:51 +0200

    Added Webhook logging to BlockModule

--- NoAdsHere/Commands/Blocks/BlockModule.cs
+++ NoAdsHere/Commands/Blocks/BlockModule.cs
@@ -2,16 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using NoAdsHere.Common;
 using NoAdsHere.Common.Preconditions;
 using NoAdsHere.Services.AntiAds;
+using NoAdsHere.Services.LogService;
 
 namespace NoAdsHere.Commands.Blocks
 {
     [Name("Blocks"), Alias("Block"), Group("Blocks")]
-    public class BlockModule : ModuleBase
+    public class BlockModule : ModuleBase<SocketCommandContext>
     {
+        private readonly LogChannelService _logChannelService;
+        private readonly DiscordShardedClient _client;
+
+        public BlockModule(LogChannelService logChannelService, DiscordShardedClient client)
+        {
+            _logChannelService = logChannelService;
+            _client = client;
+        }
+
         [Command("Enable")]
         [RequirePermission(AccessLevel.HighModerator)]
         public async Task Enable(string type)
@@ -22,20 +34,25 @@ namespace NoAdsHere.Commands.Blocks
             switch (blocktype)
             {
                 case BlockType.InstantInvite:
-                    success.Add(await AntiAds.TryEnableGuild(BlockType.InstantInvite, Context.Guild.Id)); 
+                    success.Add(await AntiAds.TryEnableGuild(BlockType.InstantInvite, Context.Guild.Id));
                     break;
+
                 case BlockType.YoutubeLink:
                     success.Add(await AntiAds.TryEnableGuild(BlockType.YoutubeLink, Context.Guild.Id));
                     break;
+
                 case BlockType.TwitchStream:
                     success.Add(await AntiAds.TryEnableGuild(BlockType.TwitchStream, Context.Guild.Id));
                     break;
+
                 case BlockType.TwitchVideo:
                     success.Add(await AntiAds.TryEnableGuild(BlockType.TwitchVideo, Context.Guild.Id));
                     break;
+
                 case BlockType.TwitchClip:
                     success.Add(await AntiAds.TryEnableGuild(BlockType.TwitchClip, Context.Guild.Id));
                     break;
+
                 case BlockType.All:
                     success.Add(await AntiAds.TryEnableGuild(BlockType.InstantInvite, Context.Guild.Id));
                     success.Add(await AntiAds.TryEnableGuild(BlockType.YoutubeLink, Context.Guild.Id));
@@ -43,6 +60,7 @@ namespace NoAdsHere.Commands.Blocks
                     success.Add(await AntiAds.TryEnableGuild(BlockType.TwitchVideo, Context.Guild.Id));
                     success.Add(await AntiAds.TryEnableGuild(BlockType.TwitchClip, Context.Guild.Id));
                     break;
+
                 default:
                     throw new ArgumentOutOfRangeException();
             }
@@ -51,13 +69,15 @@ namespace NoAdsHere.Commands.Blocks
             {
                 await ReplyAsync(
                     $"Now blocking {blocktype}. Please ensure that the bot has the 'Manage Messages' permission in the required channels.");
+                await _logChannelService.LogMessageAsync(_client, Context.Client, Emote.Parse("<:Action:333712615731888129>"),
+                    $"{Context.User} enabled {blocktype} in {Context.Guild}").ConfigureAwait(false);
             }
             else
             {
                 await ReplyAsync("Some settings were already enabled.");
             }
         }
-        
+
         [Command("Disable")]
         [RequirePermission(AccessLevel.HighModerator)]
         public async Task Disable(string type)
@@ -68,20 +88,25 @@ namespace NoAdsHere.Commands.Blocks
             switch (blocktype)
             {
                 case BlockType.InstantInvite:
-                    success.Add(await AntiAds.TryDisableGuild(BlockType.InstantInvite, Context.Guild.Id)); 
+                    success.Add(await AntiAds.TryDisableGuild(BlockType.InstantInvite, Context.Guild.Id));
                     break;
+
                 case BlockType.YoutubeLink:
                     success.Add(await AntiAds.TryDisableGuild(BlockType.YoutubeLink, Context.Guild.Id));
                     break;
+
                 case BlockType.TwitchStream:
                     success.Add(await AntiAds.TryDisableGuild(BlockType.TwitchStream, Context.Guild.Id));
                     break;
+
                 case BlockType.TwitchVideo:
                     success.Add(await AntiAds.TryDisableGuild(BlockType.TwitchVideo, Context.Guild.Id));
                     break;
+
                 case BlockType.TwitchClip:
                     success.Add(await AntiAds.TryDisableGuild(BlockType.TwitchClip, Context.Guild.Id));
                     break;
+
                 case BlockType.All:
                     success.Add(await AntiAds.TryDisableGuild(BlockType.InstantInvite, Context.Guild.Id));
                     success.Add(await AntiAds.TryDisableGuild(BlockType.YoutubeLink, Context.Guild.Id));
@@ -89,6 +114,7 @@ namespace NoAdsHere.Commands.Blocks
                     success.Add(await AntiAds.TryDisableGuild(BlockType.TwitchVideo, Context.Guild.Id));
                     success.Add(await AntiAds.TryDisableGuild(BlockType.TwitchClip, Context.Guild.Id));
                     break;
+
                 default:
                     throw new ArgumentOutOfRangeException();
             }
@@ -97,13 +123,15 @@ namespace NoAdsHere.Commands.Blocks
             {
                 await ReplyAsync(
                     $"No longer blocking {blocktype}.");
+                await _logChannelService.LogMessageAsync(_client, Context.Client, Emote.Parse("<:Action:333712615731888129>"),
+                    $"{Context.User} disabled {blocktype} in {Context.Guild}").ConfigureAwait(false);
             }
             else
             {
                 await ReplyAsync("Some settings were already disabled.");
             }
         }
-        
+
         private static BlockType ParseBlockType(string type)
         {
             switch (type)
